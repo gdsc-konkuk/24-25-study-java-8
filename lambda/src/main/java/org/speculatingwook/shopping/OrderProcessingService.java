92d310b187239354e171cea6a25ae054b0dac6ec
jay <rlaakswo0687@gmail.com>
2024-10-31 21:53:22 +0900

    All test method done

--- lambda/src/main/java/org/speculatingwook/shopping/OrderProcessingService.java
+++ lambda/src/main/java/org/speculatingwook/shopping/OrderProcessingService.java
@@ -32,26 +32,26 @@ public class OrderProcessingService {
 
     // 가장 많이 주문된 제품을 찾습니다.
     // 일단 제대로 된 값이 나올 수 있도록 streamAPI를 사용해서 작성해보자. 이 코드에서 발생할 수 있는 문제가 있을까?
+    // -> 가장 많이 주문된 제품이 여러개 나올 수 도 있다.
     public Product findMostOrderedProduct() {
         return orders.stream()
                 .map(Order::getProducts)
                 .flatMap(List::stream)
                 .collect(Collectors.groupingBy(p->p, Collectors.counting()))
-                .entrySet()
-                .stream()
+                .entrySet().stream()
                 .max(Comparator.comparingLong(Map.Entry<Product,Long>::getValue))
                 .map(Map.Entry::getKey)
                 .orElse(null);
     }
 
-    // 특정 기간 동안의 일일 매출을 계산합니다.
+    // 특정 기간 동안의 일일 매출을 계산 합니다.
     public Map<LocalDate, Double> calculateDailySales(LocalDate startDate, LocalDate endDate) {
         return orders.stream()
                 .filter(order -> startDate.minusDays(1).isBefore(order.getOrderDate()) && endDate.plusDays(1).isAfter(order.getOrderDate()))
                 .collect(Collectors.groupingBy(Order::getOrderDate, Collectors.summingDouble(Order::getTotalPrice)));
     }
 
-    // 주문 상태를 업데이트합니다.
+    // 주문 상태를 업데이트 합니다.
     public void updateOrderStatus(String orderId, UnaryOperator<String> statusUpdater) {
         orders.stream()
                 .filter( order -> order.getId().equals(orderId))
@@ -78,14 +78,15 @@ public class OrderProcessingService {
 
     // 주어진 기간 동안 가장 많은 매출을 올린 고객을 찾습니다.
     // 질문: String은 이 함수에서 사용하기 좋은 타입인가? 만약 아니라면 어떻게 바꾸는 게 더 좋을까?
+    // -> 이 결과는 List 가 될 수 있는 것 같음. List<String> 이런식으로 수정!
     public String findTopCustomer(LocalDate startDate, LocalDate endDate) {
         return orders.stream()
                 .filter(order -> startDate.minusDays(1).isBefore(order.getOrderDate()) && endDate.plusDays(1).isAfter(order.getOrderDate()))
                 .collect(Collectors.groupingBy(Order::getCustomerId, Collectors.summingDouble(Order::getTotalPrice)))
                 .entrySet().stream()
                 .max(Comparator.comparingDouble(Map.Entry::getValue))
-                .get().getKey();
-
+                .map(Map.Entry::getKey)
+                .orElse("");
     }
 
     // 모든 주문에 대해 주어진 작업을 수행합니다.
